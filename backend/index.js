d0ce4bc6feff9f823669ddff8ef6a3b5a235dc98
jjropane <juho.j.ropanen@student.jyu.fi>
2023-11-05 14:34:15 +0200

    jatkettu osaa 3c

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const app = express()
-
+const Note = require('./models/note')
 const requestLogger = (req, res, next) => {
     console.log('Method: ', req.method)
     console.log('Path:   ', req.path)
@@ -53,15 +54,14 @@ app.post('/api/notes', (request, response) => {
         })
     }
 
-    const note = {
-        content: body.content,
-        important: body.important || false,
-        id: generateId(),
-    }
+    const note = new Note({
+        content: request.body.content,
+        important: request.body.important || false,
+    })
 
-    notes = notes.concat(note)
-
-    response.json(note)
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    })
 })
 
 app.get('/', (req, res) => {
@@ -69,18 +69,15 @@ app.get('/', (req, res) => {
 })
 
 app.get('/api/notes', (req, res) => {
-    res.json(notes)
+    Note.find({}).then(notes => {
+        res.json(notes)
+    })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-    if(note){
+    Note.findById(request.params.id).then(note => {
         response.json(note)
-    } 
-    else {
-        response.status(404).end()
-    }
+    })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -92,7 +89,7 @@ app.delete('/api/notes/:id', (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
