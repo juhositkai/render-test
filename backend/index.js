d88ae40c956a50f2f1458a9ea84cf0abc2057ee9
jjropane <juho.j.ropanen@student.jyu.fi>
2023-11-05 14:45:32 +0200

    gitignoroitu build oikein

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const unknownEndpoint = (req, res) => {
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static('dist'))
+app.use(express.static('build'))
 app.use(requestLogger)
 
 let notes = [
