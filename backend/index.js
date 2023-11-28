31be48e8ee29d0d771f5dbd9e5f102937f215e30
jjropane <juho.j.ropanen@student.jyu.fi>
2023-11-28 13:44:35 +0200

    juu emt

--- backend/index.js
+++ backend/index.js
@@ -17,8 +17,9 @@ const unknownEndpoint = (req, res) => {
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static('build'))
+app.use(express.static('dist'))
 app.use(requestLogger)
+
 
 let notes = [
     {
@@ -75,21 +76,57 @@ app.get('/api/notes', (req, res) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    Note.findById(request.params.id).then(note => {
-        response.json(note)
-    })
+    Note.findById(request.params.id)
+        .then(note => {
+            if(note){
+                response.json(note)
+            }
+            else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
+    Note.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
+})
 
-    response.status(204).end()
+app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
+
+    const note = {
+        content: body.content,
+        important: body.important,
+    }
+
+    Note.findByIdAndUpdate(request.params.id, note, {new: true})
+        .then(updatedNote => {
+            response.json(updatedNote)
+        })
+        .catch(error => next(error))
 })
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if(error.name === 'CastError'){
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
+
+app.use(errorHandler)
