358033cd05bc203380daf0b76c29a8f0babb5352
jjropane <juho.j.ropanen@student.jyu.fi>
2024-04-29 10:34:32 +0300

    jotain muutoksia

--- backend/index.js
+++ backend/index.js
@@ -20,36 +20,10 @@ app.use(express.json())
 app.use(express.static('dist'))
 app.use(requestLogger)
 
-
-let notes = [
-    {
-        id: 1,
-        content: "HTML is easy",
-        important: true
-    },
-    {
-        id: 2,
-        content: "Browser can execute only JavaScript",
-        important: false
-    },
-    {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        important: true
-    }
-]
-
-const generateId = () => {
-    const maxId = notes.length > 0
-        ? Math.max(...notes.map(n => n.id))
-        : 0
-    return maxId + 1
-}
-
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
     const body = request.body
 
-    if(!body.content){
+    if(body.content === undefined){
         return response.status(400).json({
             error: 'content missing'
         })
@@ -63,6 +37,7 @@ app.post('/api/notes', (request, response) => {
     note.save().then(savedNote => {
         response.json(savedNote)
     })
+    .catch(error => next(error))
 })
 
 app.get('/', (req, res) => {
@@ -97,18 +72,17 @@ app.delete('/api/notes/:id', (request, response) => {
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-    const body = request.body
+    const {content, important} = request.body
 
-    const note = {
-        content: body.content,
-        important: body.important,
-    }
-
-    Note.findByIdAndUpdate(request.params.id, note, {new: true})
-        .then(updatedNote => {
-            response.json(updatedNote)
-        })
-        .catch(error => next(error))
+    Note.findByIdAndUpdate(
+        request.params.id,
+        { content, important },
+        { new: true, runValidators: true, context: 'query' }
+    )
+    .then(updatedNote => {
+        response.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
 
 app.use(unknownEndpoint)
@@ -118,6 +92,9 @@ const errorHandler = (error, request, response, next) => {
 
     if(error.name === 'CastError'){
         return response.status(400).send({ error: 'malformatted id' })
+    }
+    else if (error.name === 'ValidationError'){
+        return response.status(400).json({ error: error.message })
     }
 
     next(error)
