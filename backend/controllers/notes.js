71ba1385c57c34999c60ca24b6df21addd8f1898
jjropane <juho.j.ropanen@student.jyu.fi>
2024-10-14 11:22:04 +0300

    muokattu muistiinpanojen luominen vain kirjautuneille

--- backend/controllers/notes.js
+++ backend/controllers/notes.js
@@ -1,6 +1,15 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = req => {
+    const authorization = req.get('authorization')
+    if (authorization && authorization.startsWith('Bearer ')) {
+        return authorization.replace('Bearer ', '')
+    }
+    return null
+}
 
 notesRouter.get('/', async (req, res) => {
     const notes = await Note.find({}).populate('user', { username: 1, name: 1})
@@ -20,7 +29,11 @@ notesRouter.get('/:id', async (req, res) => {
 notesRouter.post('/', async (req, res) => {
     const body = req.body
 
-    const user = await User.findById(body.userId)
+    const decodedToken = jwt.verify(getTokenFrom(req), process.env.SECRET)
+    if (!decodedToken.id) {
+        return res.status(401).json({ error: 'token invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
 
     const note = new Note({
         content: body.content,
